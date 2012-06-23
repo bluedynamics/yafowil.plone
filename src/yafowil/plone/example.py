c04dddc2e06491b8a72b075855a88e2704fc110e
Jens W. Klein <jens@bluedynamics.com>
2012-06-23 10:07:38 +0200

    step further to show all samples

--- src/yafowil/plone/example.py
+++ src/yafowil/plone/example.py
@@ -1,11 +1,38 @@
-import yafowil.plone
+from zExceptions import NotFound
+from Products.Five import BrowserView
 import yafowil.loader
 from yafowil.base import factory
-from yafowil.utils import get_example
-from .form import BaseForm
+from yafowil.utils import get_example, get_example_names
 
 
-class ExampleForm(BaseForm):
+class ExampleResponseView(BrowserView):
+
+    def __call__(self):
+        for header in self.route.get('header', []):
+            self.request.reponse.setHeader(*header)
+        return self.route.get('body', '')
+
+
+class ExampleView(BrowserView):
+
+    @property
+    def example_names(self):
+        return sorted(get_example_names())
+
+    @property
+    def example(self):
+        if self.example_name:
+            return get_example(self.example_name)
+
+    @property
+    def example_name(self):
+        if hasattr(self, '_example_name'):
+            return self._example_name
+
+    @property
+    def route(self):
+        if hasattr(self, '_route'):
+            return self._route
 
     def _form_action(self, widget, data):
         return '%s/@@yafowil_example_form' % self.context.absolute_url()
@@ -14,14 +41,12 @@ class ExampleForm(BaseForm):
         self.searchterm = data['searchterm'].extracted
 
     def prepare(self):
-        plugin_name = 'yafowil'
-        example = get_example(plugin_name)
         self.form = factory(
             u'form',
-            name=plugin_name.replace('.', '-'),
+            name=self.example_name.replace('.', '-'),
             props={
-                'action': 'yafowil_example_form/%s' % plugin_name})
-        for part in example:
+                'action': 'yafowil_examples/%s' % self.example_name})
+        for part in self.example:
             widget = part['widget']
             self.form[widget.name] = widget
         self.form['submit'] = factory(
@@ -30,3 +55,15 @@ class ExampleForm(BaseForm):
                 'label': 'submit',
                 'action': 'save',
                 'handler': lambda widget, data: None})
+
+    def publishTraverse(self, request, name):
+        if self.example_name:
+            view = ExampleResponseView(self.context, self.request)
+            view.routes = dict()
+            for part in self.example:
+                view.routes.update(part.get('routes', {}))
+            return view
+        if name not in self.example_names:
+            raise NotFound()
+        self._example_name = name
+        return self
