e9c21a074c937f48add73cd74931772391d7a01e
Jens W. Klein <jk@kleinundpartner.at>
2019-06-10 10:21:34 +0200

    example bound to browserlayer

--- src/yafowil/plone/interfaces.py
+++ src/yafowil/plone/interfaces.py
@@ -4,3 +4,8 @@ from zope.publisher.interfaces.browser import IDefaultBrowserLayer
 class IYafowilLayer(IDefaultBrowserLayer):
     """YAFOWIL related browser layer.
     """
+
+
+class IYafowilDemoLayer(IYafowilLayer):
+    """YAFOWIL demos related browser layer.
+    """
