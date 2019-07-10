2f917d60d3e2ccaaf60da4cd9cc239c80ffe0f54
Robert Niederreiter <office@squarewave.at>
2019-07-10 13:41:45 +0200

    Consider forlds order annotated with plone.autoform.directives

--- src/yafowil/plone/autoform/schema.py
+++ src/yafowil/plone/autoform/schema.py
@@ -1,6 +1,7 @@
-from collections import OrderedDict
+from odict import odict
 from operator import attrgetter
 from plone.autoform.interfaces import MODES_KEY
+from plone.autoform.interfaces import ORDER_KEY
 from plone.autoform.interfaces import WIDGETS_KEY
 from plone.autoform.widgets import ParameterizedWidget
 from plone.supermodel.interfaces import DEFAULT_ORDER
@@ -10,8 +11,17 @@ from plone.supermodel.utils import mergedTaggedValueList
 from Products.CMFPlone import PloneMessageFactory as _
 from zope.dottedname.resolve import resolve
 from zope.schema import getFieldNamesInOrder
-
 import six
+
+
+def fqn(schema, name):
+    """Helper function for full qualified field names.
+
+    :param schema: ``plone.supermodel.model.Schema`` instance.
+    :param name: Name of the field.
+    :return: full qualified field name as string.
+    """
+    return '{}.{}'.format(schema.__name__, name)
 
 
 class Fieldset(object):
@@ -32,7 +42,7 @@ class Fieldset(object):
         self.label = label
         self.description = description
         self.order = order
-        self._children = list()
+        self.data = odict()
 
     def add(self, child):
         """Add field to fieldset.
@@ -40,18 +50,19 @@ class Fieldset(object):
         :param child: ``yafowil.plone.autoform.schema.Field`` or
             ``yafowil.plone.autoform.schema.Fieldset`` instance.
         """
-        self._children.append(child)
+        # XXX: nested fieldset not yet. do we need nested fieldsets at all?
+        self.data[child.fqn] = child
 
     def __iter__(self):
         """Iterate over fields in order.
 
         :return: children iterator.
         """
-        return self._children.__iter__()
+        return self.data.itervalues()
 
     @property
     def children(self):
-        return self._children
+        return self.data.values()
 
 
 class Field(object):
@@ -84,6 +95,10 @@ class Field(object):
         self.help = self.schemafield.description
         self.required = self.schemafield.required
         # XXX: set convenience attributes if overwritten via widget.params
+
+    @property
+    def fqn(self):
+        return fqn(self.schema, self.name)
 
 
 class Widget(object):
@@ -164,6 +179,67 @@ def resolve_widget(schema_widget):
     raise RuntimeError('Unknown widget: {0}'.format(schema_widget))
 
 
+def collect_fields_order(order, schema, main_schema):
+    """Collect field order definitions for schema annotated with
+    plone.autoform.directives.
+
+    :param order: Ordered dictionary containing full qualified field names as
+        keys and order definitions as values
+    :param schema: ``plone.supermodel.model.Schema`` instance.
+    :param main_schema: ``plone.supermodel.model.Schema`` instance.
+    """
+    # collect fields order for schema
+    for o_def in mergedTaggedValueList(schema, ORDER_KEY):
+        # ignore empty order definition
+        if not o_def:
+            continue
+        # handle related field notation
+        if o_def[2] != '*':
+            # field is from the same schema, its name can be abbreviated by
+            # a leading dot
+            if o_def[2].startswith('.'):
+                o_def[2] = fqn(schema, o_def[2][1:])
+            # field is is used without a prefix, its is looked up from the
+            # main schema
+            elif o_def[2].find('.') == -1:
+                o_def[2] = fqn(main_schema, o_def[2])
+        # store order definition
+        order[fqn(schema, o_def[0])] = o_def[1:]
+
+
+def order_fieldset(order, fieldset):
+    """Order passed fieldset by order annotated with plone.autoform.directives.
+
+    :param order: Ordered dictionary containing full qualified field names as
+        keys and order definitions as values
+    :param fieldset: ``yafowil.plone.autoform.schema.Fieldset`` instance.
+    """
+    data = fieldset.data
+    for field in data.values():
+        # no order def for field name
+        field_fqn = field.fqn
+        if field_fqn not in order:
+            continue
+        relative_to = order[field_fqn][1]
+        ref = data.get(relative_to)
+        # anchor field not found
+        if not ref:
+            # XXX: log warning
+            continue
+        direction = order[field_fqn][0]
+        field = data.pop(field_fqn)
+        if direction == 'before':
+            if relative_to == '*':
+                data.insertfirst(field_fqn, field)
+            else:
+                data.insertbefore(relative_to, field_fqn, field)
+        else:
+            if relative_to == '*':
+                data.insertlast(field_fqn, field)
+            else:
+                data.insertafter(relative_to, field_fqn, field)
+
+
 # aliases from plone autoform widget modes to yafowil widget modes
 mode_aliases = {
     'input': 'edit',
@@ -181,19 +257,26 @@ def resolve_schemata(schemata):
     :return: list of ``yafowil.plone.autoform.schema.Fieldset`` instances.
     """
     # fieldset definitions
-    fieldsets = OrderedDict()
+    fieldsets = odict()
     # create default fieldset, not resolved by plone.autoform
     fieldsets['default'] = Fieldset(
         name='default',
         label=_('label_schema_default', default='Default')
     )
+    # contains fields order annotated with plone.autoform.directives
+    order = odict()
+    main_schema = None
     for idx, schema in enumerate(schemata):
         # assume first schema in list is main schema, all remaining are
         # behavior schemata
+        if idx == 0:
+            main_schema = schema
         is_behavior = idx != 0
-        # collect annotated widgets for schema
+        # collect fields order for schema
+        collect_fields_order(order, schema, main_schema)
+        # collect widgets annotated with plone.autoform.directives
         widgets = mergedTaggedValueDict(schema, WIDGETS_KEY)
-        # get annotated widget modes for schema
+        # collect widget modes annotated with plone.autoform.directives.
         modes = {
             mode[1]: mode_aliases[mode[2]]
             for mode in mergedTaggedValueList(schema, MODES_KEY)
@@ -201,16 +284,15 @@ def resolve_schemata(schemata):
         # XXX: omitted from plone.autoform.directives
         # XXX: read_permission from plone.autoform.directives
         # XXX: write_permission from plone.autoform.directives
-        # XXX: order_before from plone.autoform.directives
-        # XXX: order_after from plone.autoform.directives
         # collect all fields from schema and create ``Field`` instances
-        fields = OrderedDict()
-        for name in getFieldNamesInOrder(schema):
-            fields[name] = Field(
-                name=name,
+        fields = odict()
+        field_names = getFieldNamesInOrder(schema)
+        for field_name in field_names:
+            fields[field_name] = Field(
+                name=field_name,
                 schema=schema,
-                widget=resolve_widget(widgets.get(name)),
-                mode=modes.get(name, 'edit'),
+                widget=resolve_widget(widgets.get(field_name)),
+                mode=modes.get(field_name, 'edit'),
                 is_behavior=is_behavior
             )
         # collect fieldsets from schema and add related fields
@@ -223,5 +305,8 @@ def resolve_schemata(schemata):
         fieldset = fieldsets['default']
         for field in fields.values():
             fieldset.add(field)
+    # order fields in each fieldset
+    for fieldset in fieldsets.values():
+        order_fieldset(order, fieldset)
     # return sorted fieldset
     return sorted(fieldsets.values(), key=attrgetter('order'))
